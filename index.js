9050b529592880d6822ca81106eaa2c6f7df9c5a
JoaoVDS21 <joao.programmerS2@gmail.com>
2023-12-12 21:24:48 -0300

    Products controller and model

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.send('Furniture World!'));
 app.listen(port, () => console.log(`App running on port ${port}`));
