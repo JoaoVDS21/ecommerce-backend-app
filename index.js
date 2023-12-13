1ae9c798ffc157c19b4fc9daa854a0eec788bcc5
JoaoVDS21 <joao.programmerS2@gmail.com>
2023-12-12 22:57:14 -0300

    Route products

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
 const app = express();
 const port = 3000;
+
+const productRouter = require('./routes/products');
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("db connected"))
   .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('Furniture World!'));
-app.listen(port, () => console.log(`App running on port ${port}`));
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({limit: '10mb', extended: true}));
+
+app.use('/api/products', productRouter)
+
+app.listen(process.env.APP_PORT || port, () => console.log(`App running on port ${process.env.APP_PORT}`));
